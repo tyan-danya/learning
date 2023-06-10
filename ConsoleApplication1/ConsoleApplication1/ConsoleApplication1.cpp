e5c58f067010dcbd1ce1e1a937da74bcc52bb205
daniil.tyan <daniil.tyan.2001@mail.ru>
2023-06-10 12:06:07 +0300

    Обучение, Лабораторная 1. Вывод строки по словам

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string our_string = "Hello world hello";
+
+	for (int i = 0; i <= our_string.length(); i++) {
+		
+		/*
+			Прошлая
+		*/
+
+		if (our_string[i] == ' ') {
+			cout << "\n";
+		}
+		else {
+			cout << our_string[i];
+		}
+		 /*
+			Нынешняя
+		 */
+		if (our_string[i] == ' ') {
+			our_string[i] = '\n';
+		}
+
+		/*
+			Божественная
+		*/
+		replace(our_string.begin(), our_string.end(), ' ', '\n');
+	}
+	cout << our_string;
+}

42c75a94610e40eb0ceb049fa3d9c93be9a64e9e
daniil.tyan <daniil.tyan.2001@mail.ru>
2023-06-10 12:07:06 +0300

    Обучение, Лабораторная 3. Скопировать числовой массив в другой массив задом на перед

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    const int count = 9;
+    int intArray[count] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int intArrayReverse[count] = {};
+
+    for (int i = 0; i < count; i++) {
+        cout << i << " - " << count - i - 1 << "\n";
+
+        intArrayReverse[count - i - 1] = intArray[i];
+        cout << intArrayReverse[count - i - 1] << "\n";
+    }
+    
+}
+
+

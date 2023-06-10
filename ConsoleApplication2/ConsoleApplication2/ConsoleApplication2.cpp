be0fea011f024db927c33d9f2e98767d52f31b38
daniil.tyan <daniil.tyan.2001@mail.ru>
2023-06-10 12:06:41 +0300

    Обучение, Лабораторная 2. Реализовать поиск символа в строке

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string our_string = "Hello world Hello";
+
+    char symbol = 'l';
+
+    for (int i = 0; i <= our_string.length(); i++) {
+        if (our_string[i] == symbol) {
+            cout << i << "\n";
+        }
+    }
+
+    /*
+        2 3 9 14 15
+    */
+}
+

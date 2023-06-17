8b93172247f140b9cadfe2503f8f6a3e47c655c8
daniil.tyan <daniil.tyan.2001@mail.ru>
2023-06-17 11:44:11 +0300

    Обучение, Лабораторная 4. Сортировка массива методом пузырька

---
+++ ConsoleApplication4/ConsoleApplication1/ConsoleApplication4.cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int count = 6;
+    int intArray[count] = { 5, 6, 3, 9, 2, 4 };
+    //                    { 5, 3, 6, 2, 4, 9 }
+
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - 1; j++) {
+            if (intArray[j] > intArray[j + 1]) {
+                int temp = intArray[j];
+                intArray[j] = intArray[j + 1];
+                intArray[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < count; i++) {
+        cout << intArray[i] << "\n";
+    }
+}
